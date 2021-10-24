1bc9717e5b442021063f779913505240fa3392b7
Gleichlucas <lucas.kunz94@gmail.com>
2021-10-24 23:30:15 +0200

    nothing

--- btc_funding.py
+++ btc_funding.py
@@ -14,6 +14,6 @@ with open('btc_rates.csv', newline='') as csvfile:
         counter += 1
         sum += apy
         avg = 0
-        if sum != 0:
+        if sum != 0: 
             avg = sum / counter
         print ("the avg apy is " + str(avg) + " in the last " + str(counter) + " hours on date" + str(row[0]))
